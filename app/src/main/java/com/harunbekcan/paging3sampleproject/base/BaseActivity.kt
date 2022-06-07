4531ae8c172078cc512f3d5f1e4166cdb7c715aa
Harun Bekcan <harun.bekcan@applogist.com>
2022-06-07 19:45:49 +0300

    baseActivity and baseFragment added

--- app/src/main/java/com/harunbekcan/paging3sampleproject/base/BaseActivity.kt
+++ app/src/main/java/com/harunbekcan/paging3sampleproject/base/BaseActivity.kt
@@ -1,4 +1,20 @@
 package com.harunbekcan.paging3sampleproject.base
 
-class BaseActivity {
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VDB : ViewDataBinding> : AppCompatActivity() {
+
+    lateinit var binding: VDB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, getLayoutId())
+    }
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
 }
--- app/src/main/java/com/harunbekcan/paging3sampleproject/base/BaseFragment.kt
+++ app/src/main/java/com/harunbekcan/paging3sampleproject/base/BaseFragment.kt
@@ -1,4 +1,34 @@
 package com.harunbekcan.paging3sampleproject.base
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VDB : ViewDataBinding> : Fragment() {
+
+    lateinit var binding: VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, getLayoutId(), container, false)
+        return binding.root
+    }
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareView(savedInstanceState)
+    }
+
+    abstract fun prepareView(savedInstanceState: Bundle?)
 }
